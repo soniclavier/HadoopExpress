40f6d13f143d5d3f20fbfee0a5a92649646443f7
vishnu <vishnu.viswanath25@gmail.com>
2015-03-29 17:12:37 +0530

    jsp support and gitignore file

--- Mlserver/src/main/java/com/hex/ml/main/Main.java
+++ Mlserver/src/main/java/com/hex/ml/main/Main.java
@@ -1,13 +1,15 @@
 package com.hex.ml.main;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		String webappDirLocation = "src/main/webapp/";
-
+		System.setProperty("org.apache.jasper.compiler.disablejsr199", "false");
+		
 		Server server = new Server(8484);
 		WebAppContext root = new WebAppContext();
 
@@ -17,9 +19,16 @@ public class Main {
 
 		root.setParentLoaderPriority(true);
 
+		Configuration.ClassList classlist = Configuration.ClassList.setServerDefault(server);
+		classlist.addBefore(
+                "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration");
+		root.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+		          ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
 		server.setHandler(root);
-
 		server.start();
 		server.join();
 	}
+	
+	
 }
