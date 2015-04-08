7701ab944144a27a2a5dd33d7cb029656e544f30
soniclavier <vishnu.viswanath25@gmail.com>
2015-04-08 21:38:26 +0530

    ui changes

--- Mlserver/src/main/java/com/hex/ml/main/Main.java
+++ Mlserver/src/main/java/com/hex/ml/main/Main.java
@@ -1,6 +1,9 @@
 package com.hex.ml.main;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -11,24 +14,29 @@ public class Main {
 		System.setProperty("org.apache.jasper.compiler.disablejsr199", "false");
 		
 		Server server = new Server(8484);
-		WebAppContext root = new WebAppContext();
-
-		root.setContextPath("/");
-		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-		root.setResourceBase(webappDirLocation);
-
-		root.setParentLoaderPriority(true);
+		
+		
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		webAppContext.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		webAppContext.setResourceBase(webappDirLocation);
+		webAppContext.setParentLoaderPriority(true);
 
 		Configuration.ClassList classlist = Configuration.ClassList.setServerDefault(server);
 		classlist.addBefore(
                 "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
                 "org.eclipse.jetty.annotations.AnnotationConfiguration");
-		root.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+		webAppContext.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
 		          ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
-		server.setHandler(root);
+		
+		HandlerCollection handlers = new HandlerCollection();
+		Handler[] handlerArr = new Handler[2];
+		handlerArr[0] = webAppContext;
+		//handlerArr[1] = 
+		handlers.setHandlers(handlerArr);
+		
+		server.setHandler(webAppContext);
 		server.start();
 		server.join();
 	}
-	
-	
 }
